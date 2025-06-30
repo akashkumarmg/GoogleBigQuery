19d9e2f17a96858eed83aa5ab870684e788560be
akashkumarmg <akash.mg@skyflow.com>
2025-06-30 14:09:35 +0530

    Update gitleaks-test-file.sh

--- gitleaks-test-file.sh
+++ gitleaks-test-file.sh
@@ -1,1 +1,25 @@
+    # github-enterprise-creds-1:
+    #   url: https://github.com/argoproj
+    #   githubAppID: 1
+    #   githubAppInstallationID: 2
+    #   githubAppEnterpriseBaseUrl: https://ghe.example.com/api/v3
+    #   githubAppPrivateKey: |
+    #     -----BEGIN OPENSSH PRIVATE KEY-----
+    #     ...
+    #     -----END OPENSSH PRIVATE KEY-----
+    # https-creds:
+    #   url: https://github.com/argoproj
+    #   password: my-password
+    #   username: my-username
+    # ssh-creds:
+    #  url: git@github.com:argoproj-labs
+    #  sshPrivateKey: |
+    #    -----BEGIN OPENSSH PRIVATE KEY-----
+    #    4GkxFhD90vcNLYLInFEX6Ppy1tP
+    #    -----END OPENSSH PRIVATE KEY-----
 
+    #    -----BEGIN TEST PRIVATE KEY-----
+    #    WYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm
+    #	   o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k
+    #	   TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAe
+    #    -----END TEST PRIVATE KEY-----
